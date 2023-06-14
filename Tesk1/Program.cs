bedbb0f5b10736d618243eae0dda8787b25900f6
adya <myish1084@gmail.com>
2023-06-14 17:43:10 +0300

    Task 1

---
+++ Tesk1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int DigitNumber(int num)
+{
+    num = (num / 10) % 10;
+    return num;
+}
+
+int num1 = DigitNumber(number);
+Console.WriteLine($"Вторая цифра числа {number} -> {num1}");
