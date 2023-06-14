58222001ae177f26072db852f9b23c52d086696c
adya <myish1084@gmail.com>
2023-06-14 16:59:07 +0300

    Task 2

---
+++ Tesk2/Program.cs
@@ -0,0 +1,32 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите целое число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int DigitNumber(int num)
+{
+    int index = 1;
+    int ind = 0;
+    while (num > 1)
+    {
+        num = num / 10;
+        index = index * 10;
+
+    }
+    ind = index / 1000;
+    return ind;
+}
+
+if (number / 100 < 1)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    int digitnum = DigitNumber(number);
+    number = number / digitnum;
+    Console.WriteLine(number % 10);
+}
